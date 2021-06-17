f5f08a2ae1dee11392a706949f60da590dfa230b
PHPCoder99 <84620860+PHPCoder99@users.noreply.github.com>
2021-06-17 19:21:59 +0700

    Added a more recent version of the code

--- pelmenipedia.js
+++ pelmenipedia.js
@@ -1,8 +1,23 @@
 // Hamburger-menu
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+  navbarLinks.classList.toggle('active');
+})
+
+// Themes
+
+const oddBlocks = document.querySelectorAll("div[class^='block']:nth-child(odd), div[class*=' block']:nth-child(odd)");
+const navbar = document.querySelector(".navbar");
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-theme");
+
+btn.addEventListener("click", () => {
+    body.classList.toggle('dark');
+    navbar.classList.toggle('dark');
+    oddBlocks.forEach((block, i) => {
+      block.classList.toggle('dark');
+    });
 })
